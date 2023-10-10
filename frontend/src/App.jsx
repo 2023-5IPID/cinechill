a541a7f7a69dfe15d0ee391aada59e245d27a5b5
IFriee <51403658+IFriee@users.noreply.github.com>
2023-10-10 13:46:47 +0200

    Mise en place du Front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { redirect } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
