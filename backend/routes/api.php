589d14e10a90ad5f59a384106a1d7a51128a0998
StringNameEqualsThejazzman <Louis.cppns@gmail.com>
2023-10-25 12:03:17 +0200

    correction pour effectuer le merge

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\FilmController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\SalleController;
 
@@ -16,9 +18,20 @@ use App\Http\Controllers\SalleController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
 });
+
+route::get("index",[FilmController::class,"index"]);
+Route::get('/{id}', [FilmController::class,'show']);
+Route::post('/addfilm', [FilmController::class,"store"]);
+Route::get('/{id}/edit', [FilmController::class,'edit']);
+Route::put('/{id}', [FilmController::class,'update']);
+Route::delete('/{id}', [FilmController::class,'destroy']);
 
 Route::post('ajouterSalle',[SalleController::class,'insert']);
 Route::PUT('modifierSalle',[SalleController::class,'edit']);
